da01ea69bbb8b3fc39d56efe1ed5558d38eb00d8
seliisyl <8859@holbertonstudents.com>
2024-05-10 01:48:29 +0200

    5-print_comb2.py

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
-    print(f"{i:02d}", end=", " if i < 99 else "")
+    if i == 99:
+       print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
