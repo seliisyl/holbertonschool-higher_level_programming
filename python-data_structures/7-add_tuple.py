456b59f82703bc27299588e1ff2e028e1ec84351
seliisyl <8859@holbertonstudents.com>
2024-05-14 09:06:33 +0000

    7-add_tuple.py

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,8 +4,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Assurer que chaque tuple a au moins 2 éléments
     tuple_a = tuple_a[:2] + (0, 0)
     tuple_b = tuple_b[:2] + (0, 0)
-    
+
     # Ajouter les éléments correspondants des deux tuples
     result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    
+
     return result
