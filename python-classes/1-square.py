9045c7c86195080e392b2625991bc1da220e0449
seliisyl <8859@holbertonstudents.com>
2024-05-16 15:23:41 +0000

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+class Square:
+    """Defines a square."""
+    def __init__(self, size):
+        """Initialize data.
+
+        Args:
+            size (int): Size of the square.
+        """
+        self.__size = size
