0f500f8232196f5de461d667414d4f80d86fbd52
seliisyl <8859@holbertonstudents.com>
2024-05-26 19:06:18 +0000

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+   function file for text_indentation
+"""
+
 
 def text_indentation(text):
     """
@@ -24,15 +28,12 @@ def text_indentation(text):
     text_length = len(text)
 
     while start < text_length:
-        # Find the next special character
         end = start
         while end < text_length and text[end] not in special_characters:
             end += 1
-        # Print the segment of text without leading and trailing spaces
         print(text[start:end].strip(), end="")
 
         if end < text_length:
-            # If we found a special character, print it with two new lines
             print(text[end] + "\n")
             end += 1
         start = end
