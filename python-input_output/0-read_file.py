86207e8151e1775d3775e985cc814e3b78240f33
seliisyl <8859@holbertonstudents.com>
2024-05-27 15:15:52 +0000

    0-read_file.py

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, "r", encoding="utf-8") as file:
+        content = file.read()
+        print(content)
