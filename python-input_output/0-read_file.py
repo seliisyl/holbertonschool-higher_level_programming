eb78e84e1fd9515d3b3ee5502e430edebfdd5642
seliisyl <8859@holbertonstudents.com>
2024-05-27 16:25:02 +0000

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,7 +5,12 @@
 
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout."""
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+
+    Parameters:
+    filename (str): path to the file to be read. Defaults to an empty string.
+    """
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
         print(content)
