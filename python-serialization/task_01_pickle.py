b2c8c339fe3337c1d36747dc8c9d8b214149a448
seliisyl <8859@holbertonstudents.com>
2024-05-29 19:12:09 +0000

    task_01_pickle.py

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -6,15 +6,46 @@
 import json
 import pickle
 
+
 class CustomObject:
 
+    def __init__(self, name, age, is_student):
+        """
+        Initialize a new instance of CustomObject.
+
+        :param name: Name of the object (string)
+        :param age: Age of the object (integer)
+        :param is_student: Whether the object represents a student (boolean)
+        """
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        """
+        Display the attributes of the object in a formatted manner.
+        """
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
     def serialize(self, filename):
+        """
+        Serialize the current instance of CustomObject to a file.
+
+        :param filename: Name of the file where the object will be saved
+        """
         with open(filename, 'wb') as f:
             pickle.dump(self, f)
 
     @classmethod
-
     def deserialize(cls, filename):
+        """
+        Deserialize an instance of CustomObject from a file.
+
+        :param filename: Name of the file from which the object will be loaded
+        :return: An instance of CustomObject if successful, otherwise None
+        """
         with open(filename, 'rb') as file:
             instance = pickle.load(file)
         return instance
