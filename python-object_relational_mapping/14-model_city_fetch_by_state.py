9917add2850a5964a2fe7dcb551c69d5228c317c
seliisyl <8859@holbertonstudents.com>
2024-06-23 05:17:48 +0200

    14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
 """
-Script that prints all City objects from the database hbtn_0e_14_usa
+return all cities from database via python
+parameters given to script: username, password, database
 """
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
+from sys import argv
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: python3 14-model_city_fetch_by_state.py <username> <password> <database>")
-        sys.exit(1)
 
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Create an engine and connect to the MySQL server
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}')
-
-    # Create a configured "Session" class
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
-    # Create a Session
     session = Session()
 
-    try:
-        # Query to fetch all City objects
-        cities = session.query(City).join(State).order_by(City.id).all()
+    for q in session.query(State.name, City.id, City.name).filter(
+            State.id == City.state_id).order_by(City.id):
+        print("{:s}: ({:d}) {:s}".format(q[0], q[1], q[2]))
 
-        # Print results as required
-        for city in cities:
-            print(f"{city.state.name}: ({city.id}) {city.name}")
-
-    except Exception as e:
-        print(f"Error: {e}")
-
-    finally:
-        # Close the session
-        session.close()
+    session.close()
